a60160c9a12ad9dfccc68f066f531c989e4c9eb3
Lena Bartell <lbartell@morsecorp.com>
2020-06-27 19:12:27 -0400

    initial solution, some errors fixed

---
+++ second/test_solution.py
@@ -0,0 +1,26 @@
+from pytest import mark
+
+from second.solution import solution
+
+
+@mark.parametrize("versions, expected", [
+    (["1.11", "2.0.0", "1.2", "2", "0.1", "1.2.1", "1.1.1", "2.0"],
+     ["0.1", "1.1.1", "1.2", "1.2.1", "1.11", "2", "2.0", "2.0.0"]),
+    (["1.1.2", "1.0", "1.3.3", "1.0.12", "1.0.2"],
+     ["1.0", "1.0.2", "1.0.12", "1.1.2", "1.3.3"]),
+    (["0.0"], ["0.0"]),
+    (["1.", "0.1", "0.1"], ["0.1", "0.1", "1."]),
+    (["1.0.0", "1", "1.0"], ["1", "1.0", "1.0.0"])
+
+])
+def test_solution(versions, expected) -> None:
+    """
+    Test solution
+    """
+    # Arrange
+
+    # Act
+    result = solution(versions)
+
+    # Assert
+    assert result == expected
