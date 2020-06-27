839cb64fc8395821e506007cf169a992601119d5
Lena Bartell <lbartell@morsecorp.com>
2020-06-27 19:25:30 -0400

    final solution for elevator challenge

--- second/solution.py
+++ second/solution.py
@@ -12,4 +12,16 @@ def solution(l):
     Returns:
         ordered_versions: ordered list of elevators versions, by increasing semantic version
     """
-    return sorted(l, key=lambda ver: tuple(int(num) for num in ver.split(".")))
+    return sorted(l, key=semantic_version_tuple)
+
+
+def semantic_version_tuple(version_string):
+    """Convert semantic version string to tuple representation
+
+    Args:
+        version_string: str, version string, e.g. "1", "0.1", "1.2.3"
+
+    Returns:
+        tuple of major, minor, revision integers
+    """
+    return tuple(int(num) for num in version_string.split("."))
