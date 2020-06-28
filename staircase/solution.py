6e4ca7f805aebe971e9e799bc644e833757a00d2
Lena Bartell <lbartell@morsecorp.com>
2020-06-28 15:08:27 -0400

    attempting a different approach, not working

--- staircase/solution.py
+++ staircase/solution.py
@@ -15,17 +15,56 @@ def solution(bricks):
     """
 
     step_size_options = range(1, bricks)
-    num_valid_subsets = 0
-
-    # Get all potential staircase arrangements
-    for set_option in powerset(step_size_options):
-
-        # If this is a valid staircase arrangement
-        if sum(set_option) == bricks and len(set_option) > 1:
-
-            num_valid_subsets += 1
+    num_valid_subsets = subsets_matching_total(step_size_options, bricks)
 
     return num_valid_subsets
+
+
+def subsets_matching_total(my_list, total_sum):
+    """Return number of subset of my_list that add up to total_sum"""
+    count = 0
+
+    # If the list is short, see if it sums
+    if len(my_list) <= 2:
+        if sum(my_list) == total_sum:
+            count += 1
+
+    # Otherwise, break down the list
+    else:
+        # Add number of valid subsets of length 2
+        if my_list[0] > 1:
+            num_two_subsets = len(my_list[: - my_list[0] + 1]) // 2
+        else:
+            num_two_subsets = len(my_list) // 2
+        count += num_two_subsets
+        print str(num_two_subsets) + " tuples totaling " + str(total_sum) + " in " + str(my_list)
+
+        # Add number of valid subsets based on the first element, plus other middle elements
+        count += subsets_matching_total(
+            my_list=my_list[1: - my_list[0]],
+            total_sum=total_sum - my_list[0]
+        )
+
+    print "count: " + str(count) + " sum: " + str(total_sum) + " my_list: " + str(my_list)
+    return count
+
+
+def bruce_force_solution(bricks):
+    """The number of different staircases that can be built from exactly n bricks
+
+    Args:
+        bricks: int, number of bricks used to build the staircase, [3, 200]
+
+    Returns:
+        int, number of staircase options
+    """
+    step_size_options = range(1, bricks)
+    num_options = 0
+    for option in powerset(step_size_options):
+        if len(option) > 1 and sum(option) == bricks:
+            num_options += 1
+
+    return num_options
 
 
 def powerset(iterable):
@@ -37,6 +76,5 @@ def powerset(iterable):
     Returns:
         powerset generator
     """
-    "powerset([1,2,3]) --> () (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)"
     s = list(iterable)
     return chain.from_iterable(combinations(s, r) for r in range(len(s)+1))
--- staircase/test_solution.py
+++ staircase/test_solution.py
@@ -1,5 +1,5 @@
 from parameterized import parameterized
-from staircase.solution import solution
+from staircase.solution import solution, bruce_force_solution
 
 
 @parameterized.expand([
@@ -19,4 +19,31 @@ def test_solution(n, expected):
 
     # Assert
     assert result == expected
+
+
+# @parameterized.expand([
+#     # (3,),
+#     # (4,),
+#     # (5,),
+#     # (6,),
+#     # (7,),
+#     # (8,),
+#     (9,),
+#     # (10,),
+#     # (11,),
+# ])
+# def test_solution_brute(n):
+#     """
+#     Test solution
+#     """
+#     # Arrange
+#     expected = bruce_force_solution(n)
+#     print "Expected: " + str(expected)
+#
+#     # Act
+#     result = solution(n)
+#     print "Actual: " + str(result)
+#
+#     # Assert
+#     assert result == expected
 
