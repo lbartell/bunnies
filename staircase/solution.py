f28a0b0d32bc5f44129fd13accc10fd20a2c474b
Lena Bartell <lbartell@morsecorp.com>
2020-06-28 13:56:23 -0400

    staircase: initial solution

--- staircase/solution.py
+++ staircase/solution.py
@@ -1,7 +1,42 @@
 """
 Solution to: The Grandest Staircase Of Them All
 """
+from itertools import combinations, chain
 
 
-def solution(n):
-    pass
+def solution(bricks):
+    """The number of different staircases that can be built from exactly n bricks
+
+    Args:
+        bricks: int, number of bricks used to build the staircase, [3, 200]
+
+    Returns:
+
+    """
+
+    step_size_options = range(1, bricks)
+    num_valid_subsets = 0
+
+    # Get all potential staircase arrangements
+    for set_option in powerset(step_size_options):
+
+        # If this is a valid staircase arrangement
+        if sum(set_option) == bricks and len(set_option) > 1:
+
+            num_valid_subsets += 1
+
+    return num_valid_subsets
+
+
+def powerset(iterable):
+    """Generate powerset (the set of all subsets)
+
+    Args:
+        iterable: iterable to generate subsets from
+
+    Returns:
+        powerset generator
+    """
+    "powerset([1,2,3]) --> () (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)"
+    s = list(iterable)
+    return chain.from_iterable(combinations(s, r) for r in range(len(s)+1))
--- staircase/test_solution.py
+++ staircase/test_solution.py
@@ -3,6 +3,10 @@ from staircase.solution import solution
 
 
 @parameterized.expand([
+    (3, 1),
+    (4, 1),
+    (5, 2),
+    # (200, 487067745)
 ])
 def test_solution(n, expected):
     """
