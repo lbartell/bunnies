3451e2b7ccdc2f24b30fe8fa15c3dfad572f20a3
Lena Bartell <lbartell@morsecorp.com>
2020-06-29 19:52:25 -0400

    running with bunnies: initialize readme and solution

---
+++ running/solution.py
@@ -0,0 +1,16 @@
+"""
+Solution to: The Grandest Staircase Of Them All
+"""
+
+
+def solution(times, time_limit):
+    """Calculate the most bunnies you can pick up and which bunnies they are
+
+    Args:
+        times: 2D array of int; time it takes to move from your starting point to all of the bunnies and to the bulkhead
+        time_limit: int, time limit before bulkhead closes
+
+    Returns:
+        list of integers specifying which bunnies to save
+    """
+    pass
---
+++ running/test_solution.py
@@ -0,0 +1,19 @@
+from parameterized import parameterized
+from running.solution import solution
+
+
+@parameterized.expand([
+    ([[0, 2, 2, 2, -1], [9, 0, 2, 2, -1], [9, 3, 0, 2, -1], [9, 3, 2, 0, -1], [9, 3, 2, 2, 0]], 1, [1, 2]),
+    ([[0, 1, 1, 1, 1], [1, 0, 1, 1, 1], [1, 1, 0, 1, 1], [1, 1, 1, 0, 1], [1, 1, 1, 1, 0]], 3, [0, 1])
+])
+def test_solution(times, time_limit, expected):
+    """
+    Test solution
+    """
+    # Arrange
+
+    # Act
+    result = solution(times, time_limit)
+
+    # Assert
+    assert result == expected
